62db06cd1431b64802bbaff884a434c54d90804a
Noor fatima <fatimajutt806@gmail.com>
2020-07-26 05:00:19 -0700

    Todo-App

--- app.js
+++ app.js
@@ -4,20 +4,36 @@ function addTodo(){
 
 	var li=document.createElement('li');
 	var liText=document.createTextNode(todo_item.value);
+	li.setAttribute("class","mt-5")
 	li.appendChild(liText);
 
 	var delBtn=document.createElement("button")
     var delText=document.createTextNode("Delete");
-    delBtn.setAttribute("class","btn")
-    delBtn.setAttribute("onclick","deletItem()")
+    delBtn.setAttribute("class","ml-3")
+    delBtn.setAttribute("onclick","deleteItem(this)")
     delBtn.appendChild(delText);
 
+    var editBtn=document.createElement("button")
+    var editText=document.createTextNode("Edit");
+    editBtn.setAttribute("class","ml-3")
+    editBtn.appendChild(editText)
+    editBtn.setAttribute("onclick","editItem(this)")
+
     li.appendChild(delBtn)
+    li.appendChild(editBtn)
 
 	list.appendChild(li);
-	console.log(li)
+	todo_item.value=""
 } 
 
-function deleteItem(){
-
+function deleteItem(e){
+	e.parentNode.remove()
+}
+function editItem(e){
+	var val=e.parentNode.firstChild.nodeValue;
+	var editValue=prompt("Enter edit value",val)
+	e.parentNode.firstChild.nodeValue=editValue
+}
+function deleteAll(){
+	list.innerHTML=""
 }
